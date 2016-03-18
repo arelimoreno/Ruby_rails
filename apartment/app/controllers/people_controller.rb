e426c11356bd8ac4dae4f6315dfbda13bd6c033e
arelimoreno <areli.94moreno@gmail.com>
2016-03-17 20:36:25 -0500

    torres residenciales listas

--- apartment/app/controllers/people_controller.rb
+++ apartment/app/controllers/people_controller.rb
@@ -1,23 +1,27 @@
 class PeopleController < ApplicationController
 	def index
       @tower = Tower.find(params[:tower_id])
-     @person=@tower.home.people.all
+      @home = @tower.homes.find(params[:home_id])
+      @person=@home.people
   end
 	 def create
     @tower = Tower.find(params[:tower_id])
-    @person = @tower.home.people.create(person_params)
+    @home = @tower.homes.find(params[:home_id])
+    @person = @home.people.create(person_params)
     redirect_to tower_home_people_path
   end
 
   def edit
-  @tower = Tower.find(params[:tower_id]) 
-  @person=@tower.home.people.find(params[:id])
+  @tower = Tower.find(params[:tower_id])
+  @home = @tower.homes.find(params[:home_id])
+  @person = @home.people
     
   end
 
   def update
-    @tower = Tower.find(params[:tower_id]) 
-   @person=@tower.home.people.find(params[:id])
+    @tower = Tower.find(params[:tower_id])
+    @home = @tower.homes.find(params[:home_id])
+    @person = @home.people.create(person_params)
     @person.update(person_params)
    redirect_to tower_home_people_path
   end
@@ -25,7 +29,8 @@ class PeopleController < ApplicationController
 
  def destroy
     @tower = Tower.find(params[:tower_id])
-    @person = @tower.homes.find(params[:id])
+    @home = @tower.homes.find(params[:home_id])
+    @person = @home.people.create(person_params)
     @person.destroy
     redirect_to tower_home_people_path
   end
